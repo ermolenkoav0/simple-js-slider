f7d7f2b506cbe72eef5456d8ad5080149428d2ca
ermolenkoav0 <ermolenkoav@mer.ci.nsu.ru>
2023-10-05 16:41:39 +0700

    my first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var sliderLine = document.getElementById("slider-line");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+var position = 0;
+
+left.addEventListener('click', function(){
+    if (position == 0) position = -1800;
+    else position = position + 600;
+    sliderLine.style.left = position+"px";
+});
+
+right.addEventListener('click', function(){
+    if (position == -1800) position = 0;
+    else position = position - 600;
+    sliderLine.style.left = position+"px";
+});
